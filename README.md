62e32aef2abae592953ff7154a4597d8de09f641
elibor1 <49230597+elibor1@users.noreply.github.com>
2021-01-13 09:02:40 +0100

    ting

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Kjør koden i cmd
 cd til filplasseringen og kjør:
+
 ```
 npm install
 node index.js
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const socket = require('socket.io');
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 5000
 
 var app = express();
 const server = http.createServer(app)
@@ -14,67 +14,75 @@ app.use(express.static(__dirname + "/"));
 
 var games = Array(100);
 for (let i = 0; i < 100; i++) {
-    games[i] = {players: 0 , pid: [0 , 0]};
+  games[i] = {
+    players: 0,
+    pid: [0, 0]
+  };
 }
 
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-    // console.log(players);
-    var color;
-    var playerId =  Math.floor((Math.random() * 100) + 1)
-    
+io.on('connection', function(socket) {
+  // console.log(players);
+  var color;
+  var playerId = Math.floor((Math.random() * 100) + 1)
 
-    console.log(playerId + ' connected');
 
-    socket.on('joined', function (roomId) {
-        // games[roomId] = {}
-        if (games[roomId].players < 2) {
-            games[roomId].players++;
-            games[roomId].pid[games[roomId].players - 1] = playerId;
-        }
-        else{
-            socket.emit('full', roomId)
-            return;
-        }
-        
-        console.log(games[roomId]);
-        players = games[roomId].players
-        
+  console.log(playerId + ' connected');
 
-        if (players % 2 == 0) color = 'black';
-        else color = 'white';
+  socket.on('joined', function(roomId) {
+    // games[roomId] = {}
+    if (games[roomId].players < 2) {
+      games[roomId].players++;
+      games[roomId].pid[games[roomId].players - 1] = playerId;
+    } else {
+      socket.emit('full', roomId)
+      return;
+    }
 
-        socket.emit('player', { playerId, players, color, roomId })
-        // players--;
+    console.log(games[roomId]);
+    players = games[roomId].players
 
-        
-    });
 
-    socket.on('move', function (msg) {
-        socket.broadcast.emit('move', msg);
-        // console.log(msg);
-    });
+    if (players % 2 == 0) color = 'black';
+    else color = 'white';
 
-    socket.on('play', function (msg) {
-        socket.broadcast.emit('play', msg);
-        console.log("ready " + msg);
-    });
+    socket.emit('player', {
+      playerId,
+      players,
+      color,
+      roomId
+    })
+    // players--;
 
-    socket.on('disconnect', function () {
-        for (let i = 0; i < 100; i++) {
-            if (games[i].pid[0] == playerId || games[i].pid[1] == playerId)
-                games[i].players--;
-        }
-        console.log(playerId + ' disconnected');
 
-    }); 
+  });
 
-    
+  socket.on('move', function(msg) {
+    socket.broadcast.emit('move', msg);
+    // console.log(msg);
+  });
+
+  socket.on('play', function(msg) {
+    socket.broadcast.emit('play', msg);
+    console.log("ready " + msg);
+  });
+
+  socket.on('disconnect', function() {
+    for (let i = 0; i < 100; i++) {
+      if (games[i].pid[0] == playerId || games[i].pid[1] == playerId)
+        games[i].players--;
+    }
+    console.log(playerId + ' disconnected');
+
+  });
+
+
 });
 
 server.listen(port);
 console.log('Connected');
+console.log('Server lytter på port localhost:' + port);
